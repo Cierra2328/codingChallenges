51caeae91916c8154db65ce7a668deb923580826
Cierra2328 <ap28sooners@gmail.com>
2024-09-19 09:13:32 -0500

    comments

--- reverseString.py
+++ reverseString.py
@@ -1,3 +1,6 @@
+#Coding Challenge: * **Write a function that reverses a string.** 
+# This is a simple but challenging challenge that tests your understanding of strings and reversing them.
+
 #function and console input to reverse a string in python
 def reverse(string):
     return string[::-1]
