0fdf968391eda007d18192065c8fd243398a5a84
Cierra2328 <ap28sooners@gmail.com>
2024-09-23 10:46:20 -0500

    js challenges done

--- javaScriptChallenges.js
+++ javaScriptChallenges.js
@@ -72,10 +72,10 @@ console.log(palindrome('racecar'));
     let arraySorted = [];
     for (let x=0; x< array.length; x++){
         if(arraySorted.length === 0){
-                arraySorted.push(array[x])
+                arraySorted.push(array[x]);
         }
         else{
-            let inserted = false
+            let inserted = false;
             for(let y=0; y<arraySorted.length;y++){
                 if (array[x] < arraySorted[y]){
                     let newArray = [
@@ -90,16 +90,88 @@ console.log(palindrome('racecar'));
             }
             
             if (!inserted){
-                arraySorted.push(array[x])
+                arraySorted.push(array[x]);
             }
         
             
         }
     }
-    console.log(arraySorted)
+    console.log(arraySorted);
 }
                 
 sortArray([21, 30, 5, 16, 50]);
 
+///////////////////////////////////////////////////////////////////////
+/*Coding Challenge #5
+* **Write a function that finds the maximum value in an array of numbers.** 
+ This is a simple but challenging challenge that tests your understanding of arrays and finding the maximum value.*/
 
+function findMax(array){
+    let num = array[0];
+    for(let x = 0;x<array.length;x++){
+        if(array[x] > num){
+            num = array[x];
+        }
+    }
+    
+    console.log(`The highest number in the array is: ${num}`);
+}
+
+findMax([50, 35, 21, 60, 95, 100]);
+
+
+///////////////////////////////////////////////////////////////////
+/*Coding Challenge #6
+* **Write a function that finds the minimum value in an array of numbers.** 
+ This is similar to the previous challenge, but it tests your understanding of finding the minimum value.*/
+
+ function findMin(array){
+    let num = array[0];
+    for(let x=0;x<array.length;x++){
+        if (array[x] < num){
+            num = array[x];
+        }
+    }
+    console.log(`The lowest number in the list is: ${num}`);
+ }
+
+findMin([22, 34, 8, 100, 3, 20]);
+
+
+//////////////////////////////////////////////////////////////////
+/*Coding Challenge #7
+#* **Write a function that checks if a number is prime.** A prime number is a number that is only divisible by itself and 1. 
+# This challenge tests your understanding of prime numbers and the logic behind checking if a number is prime.*/
+
+function findPrime(num){
+    if(num <= 1){
+        return false;
+    }
+    for (let x = 2; x <= Math.floor(Math.sqrt(num)); x++){
+        if(num % x === 0){
+            return false;
+        }
+    }
+    return true;   
+}     
+
+console.log(findPrime(1049));
+
+
+//////////////////////////////////////////////////////////////////
+/*Coding Challenge #8
+* **Find the factorial of a number.** 
+ This is a classic coding challenge that tests your understanding of recursion and factorials.*/
+
+function factorial(num){
+    let result = 1;
+    for(let x = 0; x<num;x++){
+        result = result * num;
+        num = num - 1;
+    }
+        
+    console.log(result);
+}
+
+factorial(6);
 
